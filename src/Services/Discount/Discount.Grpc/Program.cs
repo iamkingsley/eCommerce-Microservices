aaf82bddeebf4358340ac872c809513f80f99191
Bernard AFS <afs.bernardcodjoe@gmail.com>
2023-12-28 01:21:38 +0000

    Containerized Discount.Grpc microservice using existing posgres server service

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,24 @@
+using Discount.Grpc.Repositories.Interfaces;
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+using Discount.Grpc.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+// Seed coupon data
+app.MigrateDatabase<Program>();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
