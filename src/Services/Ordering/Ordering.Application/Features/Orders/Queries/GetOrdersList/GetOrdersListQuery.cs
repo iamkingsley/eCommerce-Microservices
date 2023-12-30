ac1ac1d5d00f3e277a57fae0c3485bb81122e8d3
Bernard AFS <afs.bernardcodjoe@gmail.com>
2023-12-30 18:32:59 +0000

    Developing Ordering.Application layer of Ordering microservice

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrderDto>>
+    {
+        public string  UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
